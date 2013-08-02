8d21d6cbca8d81ffea92d00ac129cbedce486883
Valery Zuban <zubey.next@gmail.com>
2013-08-02 14:20:21 +0300

    Merge 3 programs into one program.

--- etc/config.template.php
+++ etc/config.template.php
@@ -7,9 +7,6 @@ const NAH_USR_ID_ANONYMOUS = 1;
 // ---------------------------------------------------------------------------------------------------------------------
 // Automatisch gegenereerde constanten, zie get_labels.
 
-const NAH_TTG_ID_DEMO = 2; 
-const NAH_TTG_ID_FOO  = 1; 
-const NAH_WDG_ID_FOO  = 1; 
-
+/* AUTO_GENERATED_CONSTS */
 
 // ---------------------------------------------------------------------------------------------------------------------
