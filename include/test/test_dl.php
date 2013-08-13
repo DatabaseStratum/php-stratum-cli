48d974af7d34024af4aafca9ab9a5ec2e985f012
Valery Zuban <zubey.next@gmail.com>
2013-08-13 11:39:33 +0300

    Fix function call AssertFailed in the following methods: ExecuteRow0, ExecuteRow1, ExecuteSingleton0, ExecuteSingleton1.

--- include/test/test_dl.php
+++ include/test/test_dl.php
@@ -242,18 +242,18 @@ class TST_DL
   public function ExecuteRow0( $theQuery )
   {
     $result = self::Query( $theQuery );
+    $row    = $result->fetch_array( MYSQLI_ASSOC );
+    $n      = $result->num_rows;
+    $result->free();
 
-    if (!($result->num_rows==0 || $result->num_rows==1))
+    self::$ourMySql->next_result();
+
+    if (!($n==0 || $n==1))
     {
       TST_DL::AssertFailed( "Number of rows selected by query below is %d expected 0 or 1.\n%s",
                             $result->num_rows,
                             $theQuery );
     } // @codeCoverageIgnore
-
-    $row    = $result->fetch_array( MYSQLI_ASSOC );
-    $result->free();
-
-    self::$ourMySql->next_result();
 
     return $row;
   }
@@ -264,17 +264,18 @@ class TST_DL
   public function ExecuteRow1( $theQuery )
   {
     $result = self::Query( $theQuery );
-    if ($result->num_rows!=1)
+    $row = $result->fetch_array( MYSQLI_ASSOC );
+    $n = $result->num_rows;
+    $result->free();
+
+    self::$ourMySql->next_result();
+
+    if($n!=1)
     {
       TST_DL::AssertFailed( "Number of rows selected by query below is %d expected 1.\n%s",
                             $result->num_rows,
                             $theQuery );
     } // @codeCoverageIgnore
-
-    $row = $result->fetch_array( MYSQLI_ASSOC );
-    $result->free();
-
-    self::$ourMySql->next_result();
 
     return $row;
   }
@@ -301,18 +302,18 @@ class TST_DL
   public function ExecuteSingleton0( $theQuery )
   {
     $result = self::Query( $theQuery );
+    $row    = $result->fetch_array( MYSQL_NUM );
+    $n = $result->num_rows;
+    $result->free();
 
-    if (!($result->num_rows==0 || $result->num_rows==1))
+    self::$ourMySql->next_result();
+
+    if (!($n==0 || $n==1))
     {
       TST_DL::AssertFailed( "Number of rows selected by query below is %d expected 0 or 1.\n%s",
                             $result->num_rows,
                             $theQuery );
     } // @codeCoverageIgnore
-
-    $row    = $result->fetch_array( MYSQL_NUM );
-    $result->free();
-
-    self::$ourMySql->next_result();
 
     return $row[0];
   }
@@ -324,17 +325,18 @@ class TST_DL
   public function ExecuteSingleton1( $theQuery )
   {
     $result = self::Query( $theQuery );
-    if ($result->num_rows!=1)
+    $row = $result->fetch_array( MYSQL_NUM );
+    $n = $result->num_rows;
+    $result->free();
+
+    self::$ourMySql->next_result();
+
+    if ($n!=1)
     {
       TST_DL::AssertFailed( "Number of rows selected by query below is %d expected 1.\n%s",
                             $result->num_rows,
                             $theQuery );
     } // @codeCoverageIgnore
-
-    $row = $result->fetch_array( MYSQL_NUM );
-    $result->free();
-
-    self::$ourMySql->next_result();
 
     return $row[0];
   }
