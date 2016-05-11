78aa8347916a436309a09a4dcd88da6fb8916ba2
Paul Water <p.r.water@setbased.nl>
2016-05-11 08:07:49 +0200

    Fixed typo in .gitignore.

---
+++ test/Application/StratumApplicationTest.php
@@ -0,0 +1,26 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Stratum\Test\Application;
+
+use SetBased\Stratum\Application\Stratum;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+//----------------------------------------------------------------------------------------------------------------------
+class StratumApplicationTest extends \PHPUnit_Framework_TestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  public function testExecute()
+  {
+    $application = new Stratum();
+    $application->setAutoExit(false);
+
+    $tester = new ApplicationTester($application);
+    $tester->run(['config file'=>'test/MySql/etc/stratum.cfg']);
+
+    $this->assertSame(0, $tester->getStatusCode(), $tester->getDisplay());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
