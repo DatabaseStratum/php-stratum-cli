28a6928b7f6a67c7d506461032cbb56e072ad932
Paul Water <p.r.water@setbased.nl>
2018-06-20 08:39:31 +0200

    Release: 0.9.65

--- src/Application/Stratum.php
+++ src/Application/Stratum.php
@@ -22,7 +22,7 @@ class Stratum extends Application
    */
   public function __construct()
   {
-    parent::__construct('stratum', '0.9.64');
+    parent::__construct('stratum', '0.9.65');
   }
 
   //--------------------------------------------------------------------------------------------------------------------
